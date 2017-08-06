47619ae079edcc5ecca2b9f6e6ce1c1e411cdf63
Bai Chan Kheo <kheob@hotmail.com>
2017-08-06 10:30:22 +1000

    finalized ui

--- src/components/Response.js
+++ src/components/Response.js
@@ -15,16 +15,17 @@ export default class Meme extends Component {
         //     border = "1px rgb(92, 184, 92) solid";
         // }
 
-        let style = {/*backgroundColor: backgroundColor,*/ padding: "20px", borderRadius: "4px", /*border: border,*/ height: "100%"};
+        let style = {/*backgroundColor: backgroundColor,*/ padding: "20px", borderRadius: "4px", border: "1px solid white", height: "100%", color: "white"};
         return (
             <Col sm={6} style={{marginBottom: "20px"}}>
                 <div style={style}>
                     <div>
-                        <h4><strong>Meme description:</strong></h4>
+                        <h3><strong>Meme Description:</strong></h3>
+                        <br />
                         <p>{this.props.description}</p>
                         <br />
-                        <h4><strong>More info:</strong></h4>
-                        <a href={this.props.url} target="_blank">{this.props.url}</a>
+                        <h4><strong><a href={this.props.url} target="_blank" style={{color: "#008975", textDecoration: "underline"}}>More Information</a></strong></h4>
+                        
                         <p>{this.props.topic}</p>
                         <p>{this.props.sentiment}</p>
                     </div>
